b85ecae63dfb9c79fa68def5ad5a55f108a7b1dc
Justin <Justin@OUTERHEAVEN>
2016-06-25 18:03:35 -0400

    created employee.cs and populated with all the thingies

---
+++ Homework W2D5/Homework W2D5/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_W2D5
+{
+    class Employee
+    {
+        string empName;
+        string empEmail;
+        string empReview;
+        bool empSatisfactory;
+        int empPhone;
+        double empSalary;
+        Department empDepart;
+
+        public Employee()
+        { }
+
+        public void empRaise(double percent)
+        {
+            this.empSalary = empSalary + (empSalary * percent);
+        }
+
+        public void empReviewCreate(string mainEmpReview, bool mainEmpSatis)
+        {
+            this.empReview = mainEmpReview;
+            this.empSatisfactory = mainEmpSatis;
+        }
+    }
+}
